bd71171baf44569241d9c902d284b24d34fdffd8
Sudhars04 <sudharsbabu@gmail.com>
2021-09-16 14:40:28 +0530

    Modified Employee Class

--- Employee.java
+++ Employee.java
@@ -2,15 +2,27 @@ package com.example.java;
 
 public class Employee {
 
-    public String getEmpname() {
-        return empname;
-    } private String empid;
+    private String empid;
     private String empname;
-    public Employee(String empid,String empname){
+
+    public Employee(String empid, String empname) {
         this.empid = empid;
         this.empname = empname;
     }
+
     public String getEmpid() {
         return empid;
+    }
+
+    public void setEmpid(String empid) {
+        this.empid = empid;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
     }
 }
